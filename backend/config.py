c95cb39bb86327fe6c8e33fcd7f8c936118cdce5
caioemmendes <caiom3640@gmail.com>
2025-10-03 00:27:31 -0300

    deletion of all credentials and implementation a .env

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,13 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
+load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'mysql+mysqlconnector://root@127.0.0.1:3306/calendly_like'
+        'DATABASE_URL'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', '')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
